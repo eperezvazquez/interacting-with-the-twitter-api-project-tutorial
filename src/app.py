88f1c8cb00aee19c6d6dad4b18e38e5fd9a2e16d
eperezvazquez <perezelisa21@gmail.com>
2022-06-26 22:59:11 +0000

    Sala 3 con Leonardo v1.0

--- src/app.py
+++ src/app.py
@@ -1,8 +1,38 @@
 import os
+import request 
+import tweepy
+import pandas as pd 
 from dotenv import load_dotenv
+
 
 # load the .env file variables
 load_dotenv()
 
+consumer_key = os.env.get("CONSUMER_KEY")
+consumer_secret = os.env.get("CONSUMER_SECRET")
+bearer_token= os.env.get("BEARER_TOKEN")
+
+# Creando cliente de Twitter
+
+client = tweepy.Client( bearer_token=bearer_token, 
+                        consumer_key=consumer_key, 
+                        consumer_secret=consumer_secret,
+                        return_type = requests.Response,
+                        wait_on_rate_limit=True)
+
+# Definiendo el query para Tweeter
+
+query = '#100daysofcode (pandas OR python) -is:retweet'      
+
+tweets = client.search_recent_tweets(query=query, 
+                                    tweet_fields=['author_id','created_at','lang'],
+                                     max_results=100)
+
+
+
+
+
+
 
 # your app code here
+
